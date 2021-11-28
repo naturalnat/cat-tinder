0ae4eda00825a98f97867426132a97b2c6c3bc4b
Natalie Taktachev <ntaktach@gmail.com>
2021-11-28 15:49:19 -0500

    connect cats component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cats from './components/Cats'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Cats/>
       </div>
     );
   }
---
+++ src/components/Cats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cats extends React.Component {
+
+    render() {
+        return "meow"
+    }
+
+}
+
+export default Cats; 
