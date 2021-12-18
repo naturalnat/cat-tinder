f9f88778456c85714ccc3ddf25bfb1734745a93a
Natalie Taktachev <ntaktach@gmail.com>
2021-12-18 18:47:24 -0500

    make displaycat component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Cats from './components/Cats'
+import DisplayCat from './components/DisplayCat';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>Cat Tinder</h1>
+        <DisplayCat />
         <Cats/>
       </div>
     );
---
+++ src/components/DisplayCat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CatCard from './CatCard'
+
+function DisplayCat() {
+    return (
+    <div>
+         <CatCard />
+         <button>Like</button>
+         <button>Dislike</button>
+    </div>
+    )
+}
+
+export default DisplayCat 
